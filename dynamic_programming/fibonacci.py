f7a868672d1d6e6f3d09b9b19de76e8ed6c7f069
Malcolm Mathew <malcolmmmathew@gmail.com>
2016-12-07 22:33:46 -0500

    Finished dynamic implementation of fibonacci

--- dynamic_programming/fibonacci.py
+++ dynamic_programming/fibonacci.py
@@ -6,6 +6,16 @@ def fib_recursive(n):
 
 	return fib_recursive(n-2) + fib_recursive(n-1)
 
+max_n = 45
+unknown = -1
+arr = [unknown for unknown in range(max_n)]
+arr[0] = 0
+arr[1] = 1
 
+def fib_dynamic(n):
+	if arr[n] == unknown:
+		arr[n] = fib_dynamic(n-1) + fib_dynamic(n-2)
+	return arr[n]
 
 print fib_recursive(3)
+print fib_dynamic(4)
